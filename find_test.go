6802676cbe2c879aa68d594f7fca9eebaba1e2a4
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-19 23:23:14 +0530

    Moved parse to find, added command

---
+++ find_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Parse_All(t *testing.T) {
+	var f Find
+	url, err := f.Get()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "https://rbidocs.rbi.org.in/rdocs/content/docs/68774.xls", url)
+
+	err = f.Download("https://rbidocs.rbi.org.in/rdocs/Content/DOCs/IFCB2009_02.xls", "IFCB2009_02.xls")
+	assert.Nil(t, err)
+}
