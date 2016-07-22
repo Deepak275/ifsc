a3a8cd65a8ba58b199b89e7c3df1594fa3828443
Sudhanshu Raheja <sudhanshu@leftshift.io>
2016-07-22 23:28:42 +0530

    Moved functionality from router to a new store class, added utils to find if files exist and added tests for store

---
+++ test/store.js
@@ -0,0 +1,101 @@
+var expect = require("chai").expect;
+var Store = require("../lib/store")
+var fs = require('fs')
+var utils = require('../lib/utils')
+
+describe("Store", function() {
+
+  it("storeExists() and staleDump()", function(done) {
+    let s = new Store({ diskPath: "test/ifsc_notexist.xls" })
+    s.storeExists((err, stats, exists) => {
+      expect(err.code).to.be.equal("ENOENT")
+      expect(stats).to.be.an('undefined')
+      expect(exists).to.be.false
+    })
+
+    s = new Store({ diskPath: "test/ifsc.xls" })
+    s.storeExists((err, stats, exists) => {
+      expect(err).to.be.null
+      expect(stats).to.not.be.null
+      expect(exists).to.be.true
+
+      // Find our how old is the file
+      let currentTime = new Date().getTime()
+      let modifiedTime = stats.mtime.getTime()
+      let days = Math.abs(currentTime - modifiedTime) / (1000 * 60 * 60 * 24)
+
+      staleFalse = s.staleDump(stats, Math.ceil(days + 2))
+      expect(staleFalse).to.be.false
+
+      staleFalse = s.staleDump(stats, (Math.floor(days - 2) > 0 ? Math.floor(days - 2) : 0) )
+      expect(staleFalse).to.be.true
+
+      done()
+    })
+  })
+
+  it("parseStores() and getStore()", function() {
+    let s = new Store({ diskPath: "test/ifsc.xls" })
+    s.parseStores()
+    store = s.getStore()
+    expect(store.branches.length).to.be.equal(4)
+  })
+
+  it("downloadDump()", function(done) {
+    this.timeout(3000)
+    let filePath = "test/ifsc_32mb.xls"
+
+    // Delete old file if it exists
+    if (utils.fileExists(filePath)) {
+      fs.unlinkSync(filePath)
+    }
+
+    // Set timeout to stop file download after 2 seconds
+    setInterval(() => {
+      fileExists = utils.fileExists(filePath)
+      expect(fileExists).to.be.true
+
+      // Delete file after completion of the test
+      if (utils.fileExists(filePath)) {
+        fs.unlinkSync(filePath)
+      }
+
+      done()
+    }, 2000)
+
+    // Start file download
+    let s = new Store({ diskPath: filePath })
+    s.downloadDump((err) => {
+      expect(err).to.be.null
+    })
+
+  })
+
+  it("readLocalStore()", function(done) {
+    let filePath = "test/ifsc.xls"
+    let s = new Store({ diskPath: filePath })
+    s.readLocalStore((err, branches) => {
+      expect(err).to.be.null
+      expect(branches.branches.length).to.be.equal(4)
+      done()
+    })
+  })
+
+  it("readLocalStore2() with no file", function(done) {
+    this.timeout(0)
+    filePath = "test/ifsc_32mb.xls"
+
+    // Delete old file if it exists
+    if (utils.fileExists(filePath)) {
+      fs.unlinkSync(filePath)
+    }
+
+    s = new Store({ diskPath: filePath })
+    s.readLocalStore((err, branches) => {
+      expect(err).to.be.null
+      expect(branches.branches.length).to.be.equal(132044)
+      done()
+    })
+  })
+
+})
